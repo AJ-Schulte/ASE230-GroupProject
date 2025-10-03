33cd0fbef9b7c08b078eab884d33aeacb8bf95d7
JosephAGallucci <100083354+JosephAGallucci@users.noreply.github.com>
2025-10-03 13:59:10 -0400

    Update login.php

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
     <body>
         <?php
 
-        if ($_GET["name"] != null)
+        if ($_GET["name"] != null) //GET perhaps might be swapped out as things get developed more
         {   
             echo "You are ", $_GET["name"], ". <br/> Fun name, right?<br/>";
             $username = json_encode($_GET["name"]);
